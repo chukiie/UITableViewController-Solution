66d0c6dcae1469564f51bd6dd21f93b47f380162
chukiie <chukieq@gmail.com>
2014-04-28 16:02:43 -0400

    created project

---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Charlie Quimado on 4/28/14.
+//  Copyright (c) 2014 CQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Charlie Quimado on 4/28/14.
+//  Copyright (c) 2014 CQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
